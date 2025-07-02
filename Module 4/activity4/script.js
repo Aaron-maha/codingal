83e0a744314b7e1bf8a81057312fdbc2006f4397
Aaron-maha <aaronmaharjan80@gmail.com>
2025-07-02 20:26:40 +0545

    submit

---
+++ Module 4/activity4/script.js
@@ -0,0 +1,22 @@
+function validateform(e){
+    e.preventDefault();
+    const email=document.getElementById("email").value;
+    const pass=document.getElementById("password").value;
+    const age=document.getElementById("age").value;
+    const msgBox=document.getElementById("massage");
+    let Message="";
+    if(email===''){
+        Message='Enter an email';
+        msgBox.style.color="red";
+    } else if(pass===''){
+        Message='Enter a password';
+        msgBox.style.color="red";
+    } else if(age===''){
+        Message='Enter your age';
+        msgBox.style.color="red";
+    }else{
+        Message='Login successfully';
+        msgBox.style.color="green";
+    }
+    msgBox.innerText=Message;
+}
